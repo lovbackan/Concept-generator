94f52180ab7a30f04a7d4597d96022daa66b6691
lovbackan <simon@lovbacka.se>
2022-10-31 15:50:55 +0100

    Removed my solution since it did not fix the bug

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-if ((history = !null)) {
-  // Hämta historiken
-  const history = localStorage.getItem('history').split(',');
+// Hämta historiken
+let history = localStorage.getItem('history').split(',');
 
-  // Loopa igenom historik och visa upp på hemsidan
-  const ol = document.querySelector('.historyList');
-  history.forEach((historyItem, i) => {
-    if (i <= 14) {
-      const li = document.createElement('li');
-      li.appendChild(document.createTextNode(historyItem));
-      ol.appendChild(li);
-    }
-
-    //Begränsar historik arrayen
-
-    if (history[i] > history[15]) {
-      history.pop();
-    }
-  });
-
-  // Ta nuvarande sträng
-  const newGeneratedString = document
-    .querySelector('.generatedString')
-    .innerHTML.trim();
-
-  // Lägg till ny sträng i historiken längst fram
-  if (newGeneratedString) {
-    history.unshift(newGeneratedString);
+// Loopa igenom historik och visa upp på hemsidan
+const ol = document.querySelector('.historyList');
+history.forEach((historyItem, i) => {
+  if (i <= 14) {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(historyItem));
+    ol.appendChild(li);
   }
 
-  // Uppdatera med ny historik
-  localStorage.setItem('history', history);
-  console.log(history);
+  //Begränsar historik arrayen
+
+  if (history[i] > history[15]) {
+    history.pop();
+  }
+});
+
+// Ta nuvarande sträng
+const newGeneratedString = document
+  .querySelector('.generatedString')
+  .innerHTML.trim();
+
+// Lägg till ny sträng i historiken längst fram
+if (newGeneratedString) {
+  history.unshift(newGeneratedString);
 }
+
+// Uppdatera med ny historik
+localStorage.setItem('history', history);
+console.log(history);
